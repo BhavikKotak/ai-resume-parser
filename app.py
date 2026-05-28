db1168bf81c6892815d250c6c53ae433a1cfb503
Bhavik Kotak <bahvikkotak@gmail.com>
2026-05-28 21:13:39 +0530

    fixed render app run

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-
+from models import db, User, Candidate, UploadHistory
 from flask import (
     Flask,
     render_template,
@@ -440,4 +440,9 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
